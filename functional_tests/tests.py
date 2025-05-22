df540058129c71ba52be27175cbd0ce18d04540b
15289776729 <3378057121@qq.com>
2025-05-22 22:10:47 +0800

    message 13:refator sleep() and i forget commit message 12

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,27 +1,35 @@
+from zlib import MAX_WBITS
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
+from django.test import LiveServerTestCase
+from selenium.common.exceptions import WebDriverException
 
-class NewVisitorTest(unittest.TestCase):
-
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
 
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:  # (2)
+            try:
+                table = self.browser.find_element(By.ID, 'id_list_table')  # (3)
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return  # (4)
+            except (AssertionError, WebDriverException) as e:  # (5)
+                if time.time() - start_time > MAX_WBITS:  # (6)
+                    raise e  # (6)
+                time.sleep(0.5)  # (5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 张三听说有一个在线待办事项的应用
         # 他去看了这个应用的首页
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # 他注意到网页标题和主标题都包含“To-Do”这两个词
         self.assertIn('To-Do', self.browser.title)
@@ -38,26 +46,22 @@ class NewVisitorTest(unittest.TestCase):
         # 他在文本框中输入了“Buy flowers”
         inputbox.send_keys('Buy flowers')  # (#2)
         
-        # 他按下回车键后，页面更新了
-        # 待办事项列表中显示了“1: Buy flowers”
-        inputbox.send_keys(Keys.ENTER)  # (#3)
-        time.sleep(1)  # (#4)
-        self.check_for_row_in_list_table('1: Buy flowers')
+        # 他按了回车键后，页面更新了
+        # 待办事项表格中显示了"1：Buy flowers"
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy flowers')
 
         # 页面中又显示了一个文本输入框，可以输入其他待办事项
-        # 他输入了“Give a gift to Lisi”
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        # 他输入了"Give a gift to Lisi"
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
         inputbox.send_keys('Give a gift to Lisi')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        # 页面再次更新，她的清单中显示了这两个事项
-        self.check_for_row_in_list_table('1: Buy flowers')
-        self.check_for_row_in_list_table('2: Give a gift to Lisi')
+        # 页面再次更新，她的清单中显示了这两个待办事项
+        self.wait_for_row_in_list_table('1: Buy flowers')
+        self.wait_for_row_in_list_table('2: Give a gift to Lisi')
 
         # 张三想知道这个网站是否会记住他的清单
         # 他得到数据时他会生成一个唯一的URL
         self.fail('Finish the test!')
 
-if __name__ == '__main__':  # (#6)
-    unittest.main()
