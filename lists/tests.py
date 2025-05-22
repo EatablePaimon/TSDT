cb55ea58fb82e089e498a191ec06de3c54469d2b
15289776729 <3378057121@qq.com>
2025-05-22 19:12:09 +0800

    message 7: Refactoring

--- lists/tests.py
+++ lists/tests.py
@@ -2,13 +2,9 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest  # (#1)
+from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
-
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
     def test_home_page_return_correct_html(self):  # (#2)
         request = HttpRequest()  # (#1)
         response = home_page(request)  # (#2)
