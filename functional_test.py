4a73244705342f90ffed41c988080846f93eb07e
15289776729 <3378057121@qq.com>
2025-05-22 20:05:40 +0800

    message 9: After refactoring the functional_tests

--- functional_test.py
+++ functional_test.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 张三听说有一个在线待办事项的应用
         # 他去看了这个应用的首页
@@ -33,24 +38,26 @@ class NewVisitorTest(unittest.TestCase):
         # 他在文本框中输入了“Buy flowers”
         inputbox.send_keys('Buy flowers')  # (#2)
         
-        # 他按下回车键，页面更新了
-        self.browser.find_element(By.ID, 'id_list_table')  # (#1)
-        inputbox.send_keys('Give a gift to Lisi')
+        # 他按下回车键后，页面更新了
+        # 待办事项列表中显示了“1: Buy flowers”
         inputbox.send_keys(Keys.ENTER)  # (#3)
-        time.sleep(10)  # (#4)
+        time.sleep(1)  # (#4)
+        self.check_for_row_in_list_table('1: Buy flowers')
 
-        # 页面中显示了他输入的“1: Buy flowers”
-        table = self.browser.find_element(By.ID, 'id_list_table')  # (#1)
-        rows = table.find_elements(By.TAG_NAME, 'tr')  # (#1)
-        self.assertIn('1: Buy flowers', [row.text for row in rows])  # (#1)
-        self.assertIn('2:Give a gidt to Lisi',[row.text for row in rows])
-        # 页面中显示了一个文本输入框，可以输入其他待办事项
-        # 他输入了“gift to girlfriend”
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')  # (#2)
-        inputbox.send_keys('gift to girlfriend')  # (#2)
+        # 页面中又显示了一个文本输入框，可以输入其他待办事项
+        # 他输入了“Give a gift to Lisi”
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Give a gift to Lisi')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        # 页面更新后，列表中显示了这两个待办事项
-        self.fail('Finish the test!')  # 还需要添加更多验证事项
+        # 页面再次更新，她的清单中显示了这两个事项
+        self.check_for_row_in_list_table('1: Buy flowers')
+        self.check_for_row_in_list_table('2: Give a gift to Lisi')
+
+        # 张三想知道这个网站是否会记住他的清单
+        # 他得到数据时他会生成一个唯一的URL
+        self.fail('Finish the test!')
 
 if __name__ == '__main__':  # (#6)
     unittest.main()
